f06faa33f573765fa020ec30e850a00e75e4c78d
Keven Leone <keven.leone@liferay.com>
2022-06-09 18:01:39 -0300

    Add PM2 and update projects

--- packages/solutions-core/src/controllers/Dashboard.ts
+++ packages/solutions-core/src/controllers/Dashboard.ts
@@ -1,8 +1,35 @@
+import axios from "axios";
 import { Request, Response } from "express";
 import { projects } from "../model";
 
+const FIVE_SECONDS = 5000;
+
 class Dashboard {
   async index(_request: Request, response: Response) {
+    const responsex = await axios.get("http://192.168.109.239:8081");
+
+    const projectsPromise = (projects as { address: string }[]).map((project) =>
+      axios.get(project.address, { timeout: FIVE_SECONDS })
+    );
+
+    const results = await Promise.allSettled(projectsPromise);
+
+    results.forEach((result, index) => {
+      if (result.status === "fulfilled") {
+        projects[index].status = [200, 201].includes(result.value.status)
+          ? "active"
+          : "unactive";
+
+        return;
+      }
+
+      projects[index].status = "unactive";
+    });
+
+    results;
+
+    console.log(responsex.status);
+
     response.render("index", {
       title: "Solutions Dashboard",
       projects,
